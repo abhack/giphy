8aeda65eaf733bb93fd1df0af6a52c827eafe483
Blake Hacker <blake.hacker@gmail.com>
2018-09-09 14:04:17 -0700

    add JSON call and loop through array

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var query = ''
 var key = 'ecIHIIxXu0erhdpsCv4omv5fmNQn7ixD'
-var gif = $.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${key}&limit=5`);
-gif.done(function(data) { console.log("success got data", data); });
+var url = ''
+// gif.done(function(data) { console.log("success got data", data); });
 
 
+function searchOnClick() {
+    query = $('#query').val()
+    console.log(query)
+    url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${key}`
+    event.preventDefault();
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        for (let i=0; i < data.data.length; i++){
+            console.log(data.data[i].embed_url)
+            $('#result').append(`<img src="${data.data[i].embed_url}"></img>`)
+        }
+    })
+    .catch(function(error){
+        console.log(error)
+    })  
+
+}
