f5dd8750a0fe492aad9ca42eba3b083006b319bd
Blake Hacker <blake.hacker@gmail.com>
2018-09-12 23:51:49 -0700

    updated to a more dynamic <img> tag onClick function using stored JSON object

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var query = ''
 var key = 'ecIHIIxXu0erhdpsCv4omv5fmNQn7ixD'
 var url = ''
-var giphys = {}  //store the JSON object to recall without re-running the API
+var giphyJSON = {}  //store the JSON object to recall without re-running the API
 
 
 function searchOnClick() {
@@ -16,8 +16,8 @@ function searchOnClick() {
         return response.json()
     })
     .then(function(data){
-        giphys = data
-        console.log(giphys)
+        giphyJSON = data
+        console.log(giphyJSON)
         for (let i=0; i < data.data.length; i++){
             // console.log(data.data[i].images.original.url)
             $('#results').append(`<img style= "margin:5px;height:22%;width:22%" onclick="animateImage()" src="${data.data[i].images.original_still.url}" class="${data.data[i].images.original.url}" id="${i}"></img>`)
@@ -31,13 +31,31 @@ function searchOnClick() {
 }
 
 //attempted to create a function to retrieve the animation url from the HTML element, then replace the <img> tag src attribute value
-function animateImage(){   
-    event.preventDefault();
-    $('#results img').click(function () {  // seems to sort of work, but requires a couple of initial clicks, likely because it's a callback click function within a click function
-    var gifUrl = $(this).attr('class')
-    this.setAttribute('src', gifUrl)  //boom! a little buggy, but functional after a couple clicks
 
-    /*  keeping all of this as evidence that I tried just about everything 
+//this was an attempt to update function to source from the object variable and dynamically determine whether or not to animate or stop animation (i.e. replace <img> src url) - unfortunately requires x2 clicks
+function animateImage(){   
+    $('#results img').click(function () {  // seems to sort of work, but requires a couple of initial clicks, likely because it's a callback click function within a click function
+    var obj = this
+    var imgId = parseInt($(obj).attr('id'))
+    if(obj.getAttribute('src') === giphyJSON.data[imgId].images.original_still.url){
+        obj.setAttribute('src', giphyJSON.data[imgId].images.original.url)  //boom! a little buggy, but functional after a couple clicks
+        console.log('set to animate')
+    }
+    else {
+        obj.setAttribute('src', giphyJSON.data[imgId].images.original_still.url)
+        console.log('set to still')
+    }
+
+    /*  this is the working code block for updating the url for animated images, working on resetting the stop function
+        $('#results img').click(function (){
+        this.getAttribute('src')
+        gifUrl = $(this).attr('class')
+        this.setAttribute('src', gifUrl)  //boom! a little buggy, but functional after a couple clicks
+        }
+    */
+
+    /*  
+        keeping all of this as evidence that I tried just about everything 
         var animationUrl = $('img').data('url')  //unfortunately appears to only be retrieving the first img tag values, not the unique element
         var gifId = this.key
         console.log(gifId)
